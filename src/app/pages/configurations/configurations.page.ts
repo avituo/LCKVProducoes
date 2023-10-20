d46e1fc8484f533e4d8cb65ccbadfdfcb36f284a
Vitor Pereira <a.vitu_@outlook.com>
2023-10-20 17:52:36 -0300

    Final

--- src/app/pages/configurations/configurations.page.ts
+++ src/app/pages/configurations/configurations.page.ts
@@ -9,19 +9,28 @@ import { Geolocation } from '@capacitor/geolocation';
 export class ConfigurationsPage implements OnInit {
 
   darkMode = false;
+  accountStatus = false;
+  userEmail = false;
+  username = false;
 
   constructor() { }
 
-  ngOnInit() {
+  async ngOnInit() {
     this.checkAppMode();
   }
 
   checkAppMode() {
     const checkIsDarkMode = localStorage.getItem('darkModeActivated');
+    const checkIsAccountActivated = localStorage.getItem('accountStatusActivated');
+
     checkIsDarkMode == 'true'
       ? (this.darkMode = true)
       : (this.darkMode = false);
     document.body.classList.toggle('dark', this.darkMode);
+
+    checkIsAccountActivated == 'true'
+      ? (this.accountStatus = true)
+      : (this.accountStatus = false);
   }
   
   toggleDarkMode() {
@@ -54,6 +63,40 @@ export class ConfigurationsPage implements OnInit {
       throw(e);
     }
   }
+/*
+  checkAccountInfo() {
+    const inputEmail = document.getElementById('email') as HTMLInputElement | null;
+    const inputUser = document.getElementById('user') as HTMLInputElement | null;
+
+    const valueEmail = inputEmail?.value;
+    const valueUser = inputUser?.value;
+
+    
+    inputEmail?.addEventListener('input', function (event) {
+      const targetEmail = event.target as HTMLInputElement;
+      console.log(targetEmail.value);
+    });
+    console.log(valueEmail);
+
+    inputUser?.addEventListener('input', function (event) {
+      const target = event.target as HTMLInputElement;
+      console.log(target.value);
+    });
+    console.log(valueUser);
+  }*/
+
+  changeUserEmail() {
+    this.userEmail = !this.userEmail;
+  }
+
+  changeUserName() {
+    this.username = !this.username;
+  }
+
+  changeAccountStatus() {
+    this.accountStatus = !this.accountStatus;
+    console.log(this.accountStatus);
+  }
 
   saveAll() {
     if (this.darkMode) {
@@ -62,6 +105,23 @@ export class ConfigurationsPage implements OnInit {
       localStorage.setItem('darkModeActivated', 'false');
     }
 
-  }
+    if (this.accountStatus) {
+      localStorage.setItem('accountStatusActivated', 'true');
+    } else {
+      localStorage.setItem('accountStatusActivated', 'false');
+    }
 
+    if (this.username) {
+      localStorage.setItem('username', "usuário teste");
+    } else {
+      localStorage.setItem('username', 'false');
+    }
+
+    if (this.userEmail) {
+      localStorage.setItem('userEmail', "Email@teste");
+    } else {
+      localStorage.setItem('userEmail', 'false');
+    }
+    
+  }
 }
